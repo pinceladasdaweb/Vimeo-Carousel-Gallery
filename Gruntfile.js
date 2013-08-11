cb3d0b298b87573702848d66d7aac4e33fe6e74b
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-08-11 12:48:40 -0300

    Added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'assets/js/lib.js',
+        dest: 'assets/js/lib.min.js'
+      }
+    },
+    cssmin: {
+      with_banner: {
+        options: {
+          banner: '/* Minified CSS File of <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
+        },
+        files: {
+          'assets/css/style.min.css': ['assets/css/*.css']
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Load the plugin that provides the "cssmin" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
